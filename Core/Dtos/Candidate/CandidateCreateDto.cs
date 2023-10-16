3a2fa278fc8a4a15898033d413a3e534b69766d1
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-10-16 22:54:52 +0100

    Added Dtos for Candidate Entity

---
+++ Core/Dtos/Candidate/CandidateCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos.Candidate
+{
+    public class CandidateCreateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CoverLetter { get; set; }
+        public long JobId { get; set; }
+    }
+}
