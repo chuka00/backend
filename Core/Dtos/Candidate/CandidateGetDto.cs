3ff3452824fbca2e5e97531d1f19adc8038eb4f7
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-10-20 22:27:39 +0100

    code changes

--- Core/Dtos/Candidate/CandidateGetDto.cs
+++ Core/Dtos/Candidate/CandidateGetDto.cs
@@ -12,7 +12,7 @@ namespace Core.Dtos.Candidate
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
         public string CoverLetter { get; set; }
         public string ResumeUrl { get; set; }
         public long JobId { get; set; }
